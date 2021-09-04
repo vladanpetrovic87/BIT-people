2416a74b6b7c464d9bf16fa3049defef79879857
bokiv <borislavivanisevic@gmail.com>
2021-09-04 18:39:00 +0200

    fifth commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,20 @@
 import React from "react";
 
+import './Header.css'
+
 export const Header = () => {
     
-    // const [isListView,setIsListView] = useState(false);
-    // const [isRefresh,setIsRefresh] = useState(false);
-    // useEffect(() => {
-    //     getUsers().then((users) => {
-    //         setIsListView(users) = true;
-    //     });
-    //   }, []);
-
+   
 return (
-    <nav style={{minHeight: "118px"}}>
+    <nav >
     <div className="nav-wrapper">
       <a href="#!" className="brand-logo center">
         Bit Persons
       </a>
-      <a href="#!" >About</a>
-      {/* <a
-        href="#!"
-        className="waves-effect waves-light btn"onClick={onLayoutToggle}>
-        {" "}
-        {isListView ? "List View" : "Grid View"}{" "}
+      <a href="#!" >
+          About
       </a>
-      <a
-        href="#!"
-        className="waves-effect waves-light btn"onClick={onBtnRefresh}>
-        Refresh
-      </a> */}
+      
      </div>
    
   </nav>
