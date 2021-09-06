792782c5d822abe2e1afcbb8a29239d656c59143
bokiv <borislavivanisevic@gmail.com>
2021-09-06 19:10:15 +0200

    listview with icons

--- src/components/User/components/ListView.jsx
+++ src/components/User/components/ListView.jsx
@@ -8,10 +8,10 @@ export const ListView = ( {users} ) => {
         <li className="collection-item avatar">
           <img src={element.picture.thumbnail} alt="" className="circle" />
           <div className="userData">
-            <p>name: {element.name.first}</p>
-            <p>email: {hideMail(element.email)}</p>
-            <p>
-              date of birth:{" "}
+            <p>{element.name.first} {element.name.last}</p>
+            <p><i class="material-icons">email</i>
+              email: {hideMail(element.email)}</p>
+            <p><i class="material-icons">cake</i>
               {element.dob.date.slice(0, 10).split("-").reverse().join(".")}
             </p>
           </div>
